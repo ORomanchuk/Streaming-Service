8d40f4b0ecda907a638f1df84f39077d6a01238a
Oleksandr Romanchuk <o.romanchuk95@gmail.com>
2023-03-15 03:03:49 +0200

    add Google LogIn

--- pages/signIn/form.js
+++ pages/signIn/form.js
@@ -1,179 +1,23 @@
-let signinBx = document.querySelector(".signinBx");
-let signupBx = document.querySelector(".signupBx");
-let section = document.querySelector("section");
-let container = document.querySelector(".container");
-let form1 = document.forms[0];
-let form2 = document.forms[1];
-let password = document.querySelector("#password");
-let confirmPassword = document.querySelector("#confirmPassword");
-let shipmentData = document.querySelector(".shipmentData");
-let listOfProducts = document.querySelector(".listOfProducts");
-let orderDetail = document.querySelector(".orderDetail");
-let total = document.querySelector(".total");
-let orderTotal = document.querySelector(".orderTotal");
-let hiddenList = [];
-let totalSumm = 0;
-
-let makeOrder = document.querySelector(".makeOrder").addEventListener("click", function (e) {
-    section.classList.toggle("starter")
-    signupBx.style.zIndex = "10";
-    signinBx.style.zIndex = "10";
-})
-
-let submit = document.querySelector(".submit").addEventListener("click", function (e) {
-    signupBx.style.zIndex = "10";
-    signinBx.style.zIndex = "10";
-})
-
-// let login = document.querySelector(".login").addEventListener("click", function (e) {
-//     shoppingCard.style.zIndex = "0";
-// })
-
-let back = document.querySelector(".back").addEventListener("click", function (e) {
-    alert("Not Today");
-})
-
-function toggleForm() {
-    container.classList.toggle("active")
-    section.classList.toggle("active")
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
 
+let password = document.querySelector("#password");
+let confirmPassword = document.querySelector("#confirmPassword");
 
-// PASSWORD
-// PASSWORD
-// PASSWORD
 
-form1.confirmPassword.addEventListener("change", function () {
-    if (form1.confirmPassword.value != form1.password.value) {
-        form1.confirmPassword.setCustomValidity("Введені паролі не співпадають");
-        form1.password.setCustomValidity("Введені паролі не співпадають");
+confirmPassword.addEventListener("change", function () {
+    if (confirmPassword.value != password.value) {
+        confirmPassword.setCustomValidity("Введені паролі не співпадають");
+        password.setCustomValidity("Введені паролі не співпадають");
     } else {
-        form1.confirmPassword.setCustomValidity("");
-        form1.password.setCustomValidity("");
+        confirmPassword.setCustomValidity("");
+        password.setCustomValidity("");
         shoppingCard.style.zIndex = "0";
     }
 })
-
-//   TELEPHONE
-//   TELEPHONE
-//   TELEPHONE
-
-function setCursorPosition(pos, e) {
-    e.focus();
-    if (e.setSelectionRange) e.setSelectionRange(pos, pos);
-    else if (e.createTextRange) {
-        var range = e.createTextRange();
-        range.collapse(true);
-        range.moveEnd("character", pos);
-        range.moveStart("character", pos);
-        range.select()
-    }
-}
-
-function mask(e) {
-    let matrix = this.placeholder,
-        i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, "");
-    def.length >= val.length && (val = def);
-    matrix = matrix.replace(/[_\d]/g, function (a) {
-        return val.charAt(i++) || "_"
-    });
-    this.value = matrix;
-    i = matrix.lastIndexOf(val.substr(-1));
-    i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");
-    setCursorPosition(i, this)
-}
-window.addEventListener("DOMContentLoaded", function () {
-    let input = document.querySelector("#online_phone");
-    input.addEventListener("input", mask, false);
-    input.focus();
-    setCursorPosition(3, input);
-});
-
-
-// POST DATA
-// POST DATA
-// POST DATA
-
-const sendData = async (url, data) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        body: data,
-    });
-
-    if (!response.ok) {
-        throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
-    }
-
-    return await response.json();
-}
-
-const sendCard = () => {
-    const cartForm = document.querySelector(".cartForm");
-    cartForm.addEventListener('submit', e => {
-        // e.preventDefault();
-
-        const formData = new FormData(cartForm);
-        const data = {};
-
-        for (const [key, value] of formData) {
-            data[key] = value;
-        }
-        data.order = hiddenList
-        console.log(data);
-
-        sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(data))
-            .then(() => {
-                cartForm.reset();
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    })
-}
-
-sendCard();
-
-// GET RESOURSE
-// GET RESOURSE
-// GET RESOURSE
-
-const getResourse = async (url, data) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        body: data,
-    });
-
-    if (!response.ok) {
-        throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
-    }
-
-    return await response.json();
-}
-
-const getData = () => {
-    const loginForm = document.querySelector(".loginForm");
-    loginForm.addEventListener('submit', e => {
-        // e.preventDefault();
-
-        const formData = new FormData(loginForm);
-        const data = {};
-
-        for (const [key, value] of formData) {
-            data[key] = value;
-        }
-        console.log(data);
-
-        sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(data))
-            .then(() => {
-                cartForm.reset();
-            })
-            .catch((err) => {
-                console.log(err);
-            })
-    })
-}
-
-getData();
